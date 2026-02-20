7bbed069478c158c5170fb8655294a0bffc4877b
Rui Coelho <ruicoelho@ua.pt>
2026-02-20 22:39:09 +0000

    feat: use sops secrets

--- modules/ssh-keys/variables.tf
+++ modules/ssh-keys/variables.tf
@@ -1,10 +1,22 @@
 variable "servers" {
-  description = "List of server names to generate SSH key pairs for."
+  description = "List of server names to create SSH keys for."
   type        = list(string)
 }
 
+variable "generate_keys" {
+  description = "When true, SSH key pairs are generated. When false, public keys must be provided via the sops_keys variable."
+  type        = bool
+  default     = true
+}
+
+variable "sops_keys" {
+  description = "Map of server name to public key string. Used when generate_keys is false."
+  type        = map(string)
+  default     = {}
+}
+
 variable "output_path" {
-  description = "Base directory to write SSH key files. When empty, no local files are created."
+  description = "Base directory to write SSH key files. When empty, no local files are created. Only applies when generate_keys is true."
   type        = string
   default     = ""
 }
